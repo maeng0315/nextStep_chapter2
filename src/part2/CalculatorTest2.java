da267fa26c176a4ae010616aa07b4aec9fa1da3c
DESKTOP-COO13VP\dakma <dakma@naver.com>
2022-06-29 21:30:57 +0900

    2.2.2 결과 값을 눈이 아닌 프로그램을 통해 자동화 | 49p

--- src/part2/CalculatorTest2.java
+++ src/part2/CalculatorTest2.java
@@ -9,25 +9,25 @@ class CalculatorTest2 {
     @Test
     public void add() {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(6, 3));
+        assertEquals(9, cal.add(6, 3));
     }
 
     @Test
     public void subtract() {
         Calculator cal = new Calculator();
-        System.out.println(cal.subtract(6, 3));
+        assertEquals(3, cal.subtract(6, 3));
     }
 
     @Test
     public void multiply() {
         Calculator cal = new Calculator();
-        System.out.println(cal.multiply(6, 3));
+        assertEquals(18, cal.multiply(6, 3));
     }
 
     @Test
     public void divide() {
         Calculator cal = new Calculator();
-        System.out.println(cal.divide(6, 3));
+        assertEquals(2, cal.divide(6, 3));
     }
 
 }
