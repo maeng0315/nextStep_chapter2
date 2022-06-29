5d366c914bc5ab5d080f2d609df5645ffd1a1419
DESKTOP-COO13VP\dakma <dakma@naver.com>
2022-06-29 21:47:11 +0900

    2.2.3 테스트 코드 중복 제거 | 51p

--- src/part2/CalculatorTest2.java
+++ src/part2/CalculatorTest2.java
@@ -1,33 +1,48 @@
 package part2;
 
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class CalculatorTest2 {
+public class CalculatorTest2 {
+    Calculator cal;
+
+    @Before
+    public void setup() {
+        /* 다른 테스트에 영향을 줄 수 있기 때문에, Calculator 인스턴스를 매 테스트마다 생성하여 테스트의 독립성을 보장 해줌 */
+        cal = new Calculator();
+        System.out.println("테스트 실행 전 (초기화)");
+    }
+
+    @After
+    public void teardown() {
+        System.out.println("테스트 실행 후 (후처리)\n");
+    }
 
     @Test
     public void add() {
-        Calculator cal = new Calculator();
         assertEquals(9, cal.add(6, 3));
+        System.out.println("add()");
     }
 
     @Test
     public void subtract() {
-        Calculator cal = new Calculator();
         assertEquals(3, cal.subtract(6, 3));
+        System.out.println("subtract()");
     }
 
     @Test
     public void multiply() {
-        Calculator cal = new Calculator();
         assertEquals(18, cal.multiply(6, 3));
+        System.out.println("multiply()");
     }
 
     @Test
     public void divide() {
-        Calculator cal = new Calculator();
         assertEquals(2, cal.divide(6, 3));
+        System.out.println("divide()");
     }
 
 }
